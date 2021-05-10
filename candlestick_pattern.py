1ca94e19bd1ac80a6b0afd12142852d9fceb904f
Ajay-Deshpande <ajay.d53@gmail.com>
2021-05-11 00:20:25 +0530

    updated tolerance to 1% of range

--- candlestick_pattern.py
+++ candlestick_pattern.py
@@ -5,9 +5,9 @@ ticker_data = ticker.get_historical_stock_price()
 
 def marubozu_candlestick(price_dict,tolerance=None):
     if not tolerance:
-        tolerance = 0.005 * price_dict['Close']
+        tolerance = 0.01 * (price_dict['High'] - price_dict['Low'])
     if price_dict['Close'] > price_dict['Open']:
         return int((abs(price_dict['Close'] - price_dict['High']) + abs(price_dict['Open'] - price_dict['Low'])) <= tolerance)
     return - int((abs(price_dict['Open'] - price_dict['High']) + abs(price_dict['Close'] - price_dict['Low']) <= tolerance))
 
-ticker_data['marubozu_?'] = ticker_data.apply(marubozu_candlestick,axis=1)
+ticker_data['marubozu_pattern'] = ticker_data.apply(marubozu_candlestick,axis=1)
